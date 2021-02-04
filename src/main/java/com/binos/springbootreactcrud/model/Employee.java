d881376df10c74bbf8a39513668c5d6ef76db8e7
Venus Lumanglas <vlumanglas30@gmail.com>
2021-02-04 16:14:36 +0800

    Delombok code base

--- src/main/java/com/binos/springbootreactcrud/model/Employee.java
+++ src/main/java/com/binos/springbootreactcrud/model/Employee.java
@@ -1,6 +1,7 @@
 package com.binos.springbootreactcrud.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,16 +12,9 @@ import javax.persistence.Version;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * @author Venus Lumanglas
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "employee")
 public class Employee implements Serializable {
@@ -37,8 +31,72 @@ public class Employee implements Serializable {
 	@JsonIgnore
 	private Long version;
 
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Employee employee = (Employee) o;
+		return Objects.equals(id, employee.id) && Objects.equals(firstName, employee.firstName)
+				&& Objects.equals(lastName, employee.lastName) && Objects.equals(email, employee.email)
+				&& Objects.equals(version, employee.version);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, firstName, lastName, email, version);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
 	@Override
 	public String toString() {
-		return "Item [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+		return "Employee " + "[id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", version=" + version + "]";
 	}
 }
