00d3011a1c8d58567b8062a6f7cdbe69f301d099
Venus Lumanglas <vlumanglas30@gmail.com>
2021-02-05 17:53:02 +0800

    Implement delete modal

--- src/main/js/components/Employee.js
+++ src/main/js/components/Employee.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, ButtonGroup } from 'reactstrap';
+import { Button, ButtonGroup, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import UpdateEmployeeForm from '../forms/UpdateEmployeeForm';
 
 /**
@@ -9,11 +9,17 @@ class Employee extends Component {
 
 	constructor(props) {
 		super(props);
+		this.state = { modal: false };
 		this.handleDelete = this.handleDelete.bind(this);
+		this.toggle = this.toggle.bind(this);
 	}
 
 	handleDelete() {
 		this.props.onDelete(this.props.employee);
+	}
+
+	toggle() {
+		this.setState({ modal: !this.state.modal });
 	}
 
 	render() {
@@ -27,7 +33,17 @@ class Employee extends Component {
 				<td>
 					<ButtonGroup>
 						<UpdateEmployeeForm employee={employee} attributes={attributes} onUpdate={onUpdate} />
-						<Button size="sm" color="danger" onClick={this.handleDelete}>Delete</Button>
+						<Button size="sm" color="danger" onClick={this.toggle}>Delete</Button>
+						<Modal isOpen={this.state.modal} toggle={this.toggle}>
+							<ModalHeader toggle={this.toggle}>Delete employee</ModalHeader>
+							<ModalBody>
+								Are you sure you want to delete employee {employee.entity.firstName} {employee.entity.lastName}?
+        					</ModalBody>
+							<ModalFooter>
+								<Button color="primary" onClick={this.handleDelete}>Yes</Button>{' '}
+								<Button color="secondary" onClick={this.toggle}>No</Button>
+							</ModalFooter>
+						</Modal>
 					</ButtonGroup>
 				</td>
 			</tr>
